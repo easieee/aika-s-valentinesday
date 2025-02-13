5cf58d8eb24310ec4bc1f81702692f3961a9c817
Ease <106789206+easieee@users.noreply.github.com>
2025-02-13 15:06:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var inputElement = document.querySelector('input');
+
+    inputElement.addEventListener('click', function() {
+        window.location.href = 'home.html'; // Navigate to home.html
+    });
+});
